7a22013546475f03f3efa7391e195de7ab634675
X3m4DeV <64781089+X3M4@users.noreply.github.com>
2023-11-28 19:56:18 +0100

    Delete ejercicio1.py

--- ejercicio1.py
+++
@@ -1,8 +0,0 @@
-''' Escriba un programa que pida dos números y que escriba su media aritmética.'''
-
-def ejercicio1Media(a:float, b:float):
-    c = (a+b)/2
-    print(f"La media de {a:.1f} y {b:.1f} es {c}")
-
-
-ejercicio1Media(a=float(input("Escribe el primer número\n")), b=float(input("Escribe el segundo número\n")))
