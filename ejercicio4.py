703401a8ba7a962605fe0693afee0f50d636f6af
X3m4DeV <64781089+X3M4@users.noreply.github.com>
2023-11-28 19:55:49 +0100

    Delete ejercicio4.py

--- ejercicio4.py
+++
@@ -1,7 +0,0 @@
-'''Escriba un programa que pida una temperatura en grados Celsius y que escriba esa
-temperatura en grados Fahrenheit.'''
-
-def temperaturaFahrenheit(celsius:float):
-    print(f"{celsius:.1f}ºC son {celsius*1.8+32:.1f}ºF")
-
-temperaturaFahrenheit(float(input("CONVERTIDOR DE GRADOS CELSIUS A FAHRENHEIT\nEscriba una temperatura en grados Celsius: ")))
