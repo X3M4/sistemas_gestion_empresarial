745f884e5b0bd6055eaf012865a5a375c2d8f8a1
X3m4DeV <64781089+X3M4@users.noreply.github.com>
2023-11-28 19:54:43 +0100

    Add files via upload

---
+++ T5_PYTHON_BASICO/ejercicio3.py
@@ -0,0 +1,9 @@
+'''Escriba un programa que pida una distancia en pies y pulgadas y que escriba esa
+distancia en centímetros'''
+
+def calculoCm(pies:int, pulgadas:int):
+    cm = (pies*12+pulgadas)*2.54
+    print("CONVERTIDOR DE MEDIDAS AL SISTEMA INTENACIONAL DESDE EL SISTEMA IMPERIAL\n")
+    print(f"{pies} pies y {pulgadas} pulgadas son {cm} cm")
+
+calculoCm(pies = int(input("Escriba una cantidad de pies\n")), pulgadas=int(input("Escribas una cantidad de pulgadas\n")))
