ba2ccfd3ffba702c9e773022fd0193b7a7bb40a4
X3m4DeV <64781089+X3M4@users.noreply.github.com>
2023-11-28 19:55:37 +0100

    Delete ejercicio5.py

--- ejercicio5.py
+++
@@ -1,9 +0,0 @@
-'''Escriba un programa que pida una cantidad de segundos y que escriba cuántos minutos y
-segundos son.'''
-
-def convertidorTiempo(seg:int):
-    minutos = int(seg/60)
-    segundos = seg - minutos*60
-    print(f"{seg} segundos son {minutos} minutos y {segundos} segundos")
-
-convertidorTiempo(int(input("Escriba una cantidad de segundos: ")))
