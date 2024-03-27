2f4781bbf1f8f1dabf73b9df61539991617863ee
Chema <chema@domatix.com>
2024-03-27 15:50:24 +0000

    Commit despues de los errores en el debugger

--- models/sport_team.py
+++ models/sport_team.py
@@ -1,4 +1,4 @@
-from odoo import models, fields
+from odoo import models, fields, Command
 
 class SportTeam(models.Model):
     _name = 'sport.team'
@@ -14,12 +14,17 @@ class SportTeam(models.Model):
         string='logo',
     )
     
+    number = fields.Integer(
+        string='Roster',
+        compute='_compute_number',
+    )
+    
+    
     player_list_ids = fields.One2many(
         string='Player List',
         comodel_name='sport.player',
         inverse_name='team_id',
     )
-    
     
     sport_id = fields.Many2one(
         string='Sport',
@@ -34,8 +39,15 @@ class SportTeam(models.Model):
         for record in self.player_list_ids:
             record.starting_team=False
     
+    def action_add_players_under_30(self):
+        for record in self:
+            players = self.env['sport.player'].search([('age', '<', 30), ('team_id', '=', False)])
+            players |= record.player_list_ids 
+            record.player_list_ids = [Command.set(players.ids)]
     
-    
+    def _compute_number(self):
+        for record in self:
+            record.number = len(record.player_list_ids) 
     
     
     
